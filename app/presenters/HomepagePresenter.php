bf802bc7e440f045e11bb07d61165be12a504d62
David Grudl <david@grudl.com>
2016-08-16 18:07:02 +0200

    added datagrid

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -4,14 +4,32 @@ namespace App\Presenters;
 
 use Nette;
 use App\Model;
+use Ublaboo\DataGrid\DataGrid;
 
 
 class HomepagePresenter extends BasePresenter
 {
+	/** @var Nette\Database\Context @inject */
+	public $db;
 
-	public function renderDefault()
+	public function actionDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		$grid = new DataGrid($this, 'grid');
+		$grid->setRememberState(FALSE);
+		$grid->setRefreshUrl(TRUE);
+		$grid->setPrimaryKey('id');
+		$grid->setDataSource($this->db->table('comment')->order('created'));
+
+		$grid->setItemsPerPageList([100, 500]);
+		$grid->addColumnText('article_id', 'Clanek');
+
+		$grid->addColumnText('name', 'Jmeno')
+			->setFilterText();
+
+		$grid->addColumnText('content', 'Text')
+			->setFilterText();
+
+		$this->template->grid = $grid;
 	}
 
 }
