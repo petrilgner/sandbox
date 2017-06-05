9f412ba47854bf0b1950d9ad80baaa4dd8709014
David Grudl <david@grudl.com>
2017-06-05 10:46:24 +0200

    .maintenance.php -> 503.phtml

--- www/index.php
+++ www/index.php
@@ -1,8 +1,6 @@
 <?php
 
-// Uncomment this line if you must temporarily take down your site for maintenance.
-// require __DIR__ . '/.maintenance.php';
-
 $container = require __DIR__ . '/../app/bootstrap.php';
 
-$container->getByType(Nette\Application\Application::class)->run();
+$container->getByType(Nette\Application\Application::class)
+	->run();
